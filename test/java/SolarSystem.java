833c8a7afaaa4056d72475e919d0c0f25b285bbb
Sorahon84 <shohkuch@gmail.com>
2024-11-17 11:54:58 -0600

    added planet2

--- test/java/SolarSystem.java
+++ test/java/SolarSystem.java
@@ -4,10 +4,15 @@ public class SolarSystem {
         solar.Sun="1";
         solar.Moon="1";
         solar.Planet="2";
+
         featureSun Archilies=new featureSun();
         Archilies.color="Red";
         Archilies.size="50meters";
         Archilies.temp="90000000";
+
+        planet2 nobel= new planet2();
+        nobel.temp="10000000k";
+        nobel.color="black";
     }
 
 }
